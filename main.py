290475e2cbd79a90ab8ccf3f1aaaade16f2f6adf
HoytSmith <h.smith@live.nl>
2024-11-25 21:02:18 +0100

    prints book to console

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+
+def read_book(book_path):
+    with open(book_path) as book:
+        book_text = book.read()
+        print(book_text)
+
+def main():
+    read_book("books/frankenstein.txt")
+
+main()
